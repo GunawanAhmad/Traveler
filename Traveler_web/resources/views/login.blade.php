b974b902ff20bd77253fb804129ed7e95abe72d5
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2022-03-26 14:21:52 +0700

    add login page and css design

---
+++ Traveler_web/resources/views/login.blade.php
@@ -0,0 +1,28 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700;800&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
+
+<body>
+    <main>
+        <div class="container">
+            <h1 class="font-size-72 mb-3 b-line b-line-mid">Traveler</h1>
+            <form action="">
+                <input type="email" placeholder="Email" class="input form-input mb-1 bg-primary" id="email" name="email">
+                <input type="password" placeholder="Password" class="input form-input mb-1 bg-primary" id="password"
+                    name="password">
+                <button type="submit" class="btn mx-auto d-block bg-primary text-white">Login</button>
+            </form>
+        </div>
+    </main>
+
+</html>
