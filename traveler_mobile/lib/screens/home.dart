38d888113c453704ab0ca700ecf05c64be5fb026
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2022-05-21 15:16:06 +0700

    add home screen

---
+++ traveler_mobile/lib/screens/home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('images/bgImg.png'), fit: BoxFit.cover),
+      ),
+      child: Column(
+        children: [
+          const Text(
+            "Traveler",
+            style: TextStyle(
+                fontSize: 48, color: Colors.white, fontWeight: FontWeight.w800),
+          ),
+          const Text(
+            "Explore with Our Best Tourguide",
+            style: TextStyle(
+                fontSize: 12, color: Colors.white, fontWeight: FontWeight.w400),
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 50, top: 30),
+            child: MaterialButton(
+              padding: const EdgeInsets.all(15),
+              onPressed: () {},
+              color: const Color(0xC4C4C4).withOpacity(0.5),
+              textColor: Colors.white,
+              child: const Icon(
+                Icons.chevron_right,
+                size: 32,
+              ),
+              shape: const CircleBorder(),
+            ),
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.end,
+      ),
+    ));
+  }
+}
